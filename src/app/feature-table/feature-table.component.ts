b8e809d3f85cfe1229a3dc9dc78ee24d0288eddf
Kerrykogei24 <kerrykomar@gmail.com>
2021-03-23 22:57:08 +0300

    made changes in feature table

---
+++ src/app/feature-table/feature-table.component.ts
@@ -0,0 +1,121 @@
+import { Router } from '@angular/router';
+import { IssuesService } from './../issues.service';
+import { payload, FormComponent } from './../form/form.component';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import {MatPaginator} from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import {MatTableDataSource} from '@angular/material/table';
+import { Subscription } from 'rxjs';
+// import 'rxjs/add/operator/startWith';
+import 'rxjs/add/observable/merge';
+// import 'rxjs/add/observable/fromEvent';
+// import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/distinctUntilChanged';
+import 'rxjs/add/operator/debounceTime';
+export interface dataInterface {
+  status : string,
+  PR_number: number,
+  title: string,
+  user: {
+      login: string
+  },
+  url: string
+}
+@Component({
+  selector: 'app-feature-table',
+  templateUrl: './feature-table.component.html',
+  styleUrls: ['./feature-table.component.scss'],
+  preserveWhitespaces: false
+})
+export class FeatureTableComponent implements OnInit {
+    /** Collection of subscribed variables */
+    subscriptions: Subscription[] = [];
+    /** data container */
+    data:any;
+    /** display column header */
+    displayedColumns = ['state', 'PR_number','title','user.login','url','updated_at'];
+    /** 404 error bool*/
+    dataNotFound: boolean = false;
+    /** view child containers */
+    @ViewChild(MatPaginator) paginator: MatPaginator;
+    @ViewChild(MatSort) sort: MatSort;
+    @ViewChild('filter') filter: ElementRef;
+
+    constructor (private appService: IssuesService, private router: Router, private formComponent: FormComponent) {
+    }
+
+    ngOnInit () {
+        if(this.appService.data === undefined)
+            return;
+
+        if(this.appService.data.length === 0) {
+            this.dataNotFound = true;
+        } else {
+            this.data = new MatTableDataSource(this.appService.data);
+            this.data.paginator = this.paginator;
+            this.data.sort = this.sort;
+        }
+    }
+
+    ngAfterViewInit () {
+        // listen to getting data
+        let chData: Subscription = this.appService.data$.subscribe(
+            (notifier: number) => {
+                if (notifier !== 200 || this.appService.data.length === 0) {
+                    this.dataNotFound = true;
+                    return;
+                }
+                // clear up variable
+                this.dataNotFound = false;
+                this.data = new MatTableDataSource(this.appService.data);
+                this.data.paginator = this.paginator;
+                this.data.sort = this.sort;
+            }
+        );
+        this.subscriptions.push(chData);
+        // this will be used first time sorting and pagination
+        if(this.appService.data.length !== 0) {
+            this.data.paginator = this.paginator;
+            this.data.sort = this.sort;
+        }
+    }
+
+    /**
+     * filter data
+     * @param filterValue
+     */
+    applyFilter(filterValue: string) {
+        filterValue = filterValue.trim(); // Remove whitespace
+        filterValue = filterValue.toLowerCase(); // Datasource defaults to lowercase matches
+        this.data.filter = filterValue;
+    }
+
+    /***
+     * fetching next page data and call api request
+     */
+    fetchNextPage(): void {
+        let payload:payload = {
+            orgName : '',
+            repoName : '',
+            pageNo: 1,
+            pageCount: 30
+        };
+
+        let splitArray = this.appService.data[0].repository_url.split('/');
+        payload.orgName = splitArray[4];
+        payload.repoName = splitArray[5];
+        payload.pageNo = this.appService.data.length / 30 + 1;
+
+        this.appService.fetchData(payload);
+    }
+
+    /* called when component is being destroyed
+     * clean memory or unsubscribe to current events to avoid memory leaks */
+    ngOnDestroy() {
+        this.data = [];
+        for (let subscription of this.subscriptions) {
+            subscription.unsubscribe();
+        }
+    }
+
+}
